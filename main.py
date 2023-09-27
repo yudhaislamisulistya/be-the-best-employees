da6aa825ea5d87bdf2989818cf8bed9bbc4f701d
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2023-09-27 22:26:06 +0700

    update include router

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_router, prefix="/api/v1")
+# app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router)
